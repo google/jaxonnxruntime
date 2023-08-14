43b296da85f51afa5b7c0d95856ce1e89fae0024
John QiangZhang <johnqiangzhang@google.com>
2023-08-14 09:38:33 -0700

    No public description
    PiperOrigin-RevId: 556820339

--- jaxonnxruntime/experimental/call_torch/call_torch.py
+++ jaxonnxruntime/experimental/call_torch/call_torch.py
@@ -15,6 +15,7 @@
 """Convert PyTorch function to Jax funtion."""
 
 import io
+import os
 from typing import Any, Callable, Tuple, Union
 
 from absl import logging
@@ -41,6 +42,7 @@ def call_torch(
         torch.nn.Module, torch.jit.ScriptModule, torch.jit.ScriptFunction
     ],
     args: Union[Tuple[Any, ...], torch.Tensor],
+    onnx_dump_prefix: str | None = None,
 ) -> Tuple[Callable[..., Any], Any]:
   """Give a pytorch model and return its equivilent jax function.
 
@@ -50,6 +52,7 @@ def call_torch(
   Args:
     model: the torch model to be exported.
     args:  (tuple or torch.Tensor), model inputs args for torch.onnx.export.
+    onnx_dump_prefix: The onnx_model debug directory.
 
   Returns:
     A JAX jittable function can be invoked with JAX pytree arguments.
@@ -74,6 +77,13 @@ def call_torch(
         "torch.onnx.export fails. Please debug torch.onnx.export manually"
         " first."
     ) from e
+  if onnx_dump_prefix:
+    if not os.path.exists(onnx_dump_prefix):
+      os.makedirs(onnx_dump_prefix)
+    onnx_model_file = os.path.join(onnx_dump_prefix, "model.onnx")
+    with open(onnx_model_file, "wb") as f:
+      f.write(file_obj.getvalue())
+    logging.info("Saving debug model.onnx to %s", onnx_model_file)
   file_obj.seek(0)
   onnx_model = onnx.load(file_obj)
   jax_args = jax.tree_util.tree_leaves(
