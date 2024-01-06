513215c1039f04b9bd3acc292f5615ea3db0e6fe
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-01-06 21:58:32 +0900

    fix comment

--- jaxonnxruntime/onnx_ops/flatten.py
+++ jaxonnxruntime/onnx_ops/flatten.py
@@ -55,7 +55,7 @@ class Flatten(handler.Handler):
   def version_9(
       cls, node: onnx_node.OnnxNode, inputs: Sequence[Any]
   ) -> Callable[..., Any]:
-    """ONNX version_1 Flatten op."""
+    """ONNX version_9 Flatten op."""
     cls._prepare(node, inputs, onnx_flatten)
     return onnx_flatten
 
