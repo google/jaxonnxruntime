9dfa64e1a4e5420e4c2aa54ce83d9d13a8aadc6d
Nicolas Küchler <nkuchler@google.com>
2025-01-07 18:20:07 +0000

    fix scatternd

--- jaxonnxruntime/onnx_ops/scatternd.py
+++ jaxonnxruntime/onnx_ops/scatternd.py
@@ -19,7 +19,16 @@ class ScatterND(handler.Handler):
   def _prepare(
       cls, node: onnx_node.OnnxNode, inputs: Sequence[Any], onnx_jax_impl: Any
   ):
-    node.attrs_dict["reduction"] = node.attrs.get("reduction", None)
+    reduction_map = {
+        None: "set",
+        "add": "add",
+        "mul": "multiply",
+        "max": "max",
+        "min": "min",
+    }
+    node.attrs_dict["reduction"] = reduction_map[
+        node.attrs.get("reduction", None)
+    ]
 
   @classmethod
   def version_11(
@@ -92,42 +101,56 @@ def onnx_scatternd(*input_args, reduction: str):
   k = indices.shape[-1]
   r = len(data.shape)
   q = len(indices.shape)
+  assert len(updates.shape) == q - 1 + r - k
 
   assert (
       indices.shape[: q - 1] == updates.shape[: q - 1]
-  ), "first dimensions of indicies and updates must match"
+  ), "first q-1 dims of indicies and updates must match"
   assert (
       data.shape[k:] == updates.shape[q - 1 :]
   ), "last dimensions of data and updates must match"
   assert r - k >= 0, f"expected non-negative but was: {r-k}"
-  assert len(updates.shape) == q - 1 + r - k
 
-  # flatten z indices (z1, z2, ..., zn, k) -> (z, k)
+  # flatten indices (j1, j2, .., j_{q-1}, k) -> (z, k)
   indices = jnp.reshape(indices, (-1, k))
   z = indices.shape[0]
 
-  # (z, k) -> (k, z) -> (k, z, 1, ..., 1)
+  # (z, k) -> (k, z) -> (k, z, 1,..,1)
   indices = jnp.transpose(indices)
   assert indices.shape == (k, z)
   indices = jnp.expand_dims(indices, axis=list(range(2, 2 + r - k)))
-
-  # (k, z, 1, ..., 1) -> [(z, 1, ..., 1), (z, 1, ..., 1), ...] with len k
+  # (k, z, 1,..,1) -> [(z, 1,..,1), (z, 1,..,1), ..] with len k
   indices = list(indices)
 
-  # reshape updates (z1, z2, ..., zn, x1, x2, ...) -> (z, x1, x2, ...)
+  # reshape updates (j1, j2, .., j_{q-1}, x1, x2, ...) -> (z, x1, x2, ...)
   updates_shape = (z,) + updates.shape[q - 1 :]
   updates = jnp.reshape(updates, updates_shape)
 
-  # e.g., [(range(n0),1,1), (1,range(n1),1), (1,1,range(n2))] for 3d data
+  # e.g., for (r-k)=3 and z updates:
+  # [(1,1,1,1), (1,range(x1),1,1), (1,1,range(x2),1), (1,1,1,range(x3))]
+  #   L----------L------------------L------------------L----> dim for z
   idx = jnp.meshgrid(
-      *(jnp.arange(n) for n in data.shape), sparse=True, indexing="ij"
+      *(jnp.arange(n) for n in [1] + list(data.shape[k:])),
+      sparse=True,
+      indexing="ij",
+  )
+  assert idx[0].ndim == (r - k) + 1
+
+  # idx in `data.at[idx]` is a tuple of length data.ndim, where each element
+  # is an array containing indices for a specific dimension of the data.
+  # The first k dimensions contain the filters from `indices`,
+  # the remaining dimensions select all entries.
+  # [(z,1,1,1), (z,1,1,1)..] + [(1,range(x1),1,1), (1,1,range(x2),1), (1,1,1,range(x3))]
+  # ------------k----------      ----------------------(r-k)---------------------------
+  idx = indices + idx[1:]
+  assert set(i.ndim for i in idx) == {data.ndim}
+  select_shape = jnp.broadcast_shapes(*[i.shape for i in idx])
+  assert select_shape == updates.shape, (
+      "Shape of index-selected data must match the shape of the update:"
+      f" {select_shape}!={updates.shape}."
   )
 
-  # in first k dimensions, instead of selecting all elems with (range(n0),1,1),
-  # we only select the ones specified by indices
-  for i in range(k):
-    idx[i] = indices[i]
-
-  out = getattr(data.at[tuple(idx)], reduction or "set")(updates)
-
+  out = getattr(data.at[tuple(idx)], reduction)(
+      updates, indices_are_sorted=False
+  )
   return out
